80a23936d921a4ed2e8dad8fc1bcc8f9fd099177
MuneerAzam <muneerazam14@gmail.com>
2026-09-14 20:57:32 +0530

    Add: search-2d-matrix - submission-2

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-2.py
@@ -0,0 +1,17 @@
+import numpy as np
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        nums=np.array(matrix)
+        nums=nums.flatten()
+        left=0
+        right=len(nums)-1
+        while left<=right:
+            mid=right+left//2
+            if nums[mid]>target:
+                right=mid-1
+            elif nums[mid]<target:
+                left=mid+1
+            else:
+                return True
+        return False 
+        
